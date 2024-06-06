ecf64790fb9e2495dd15387626ba0d6b168b7b5e
BenayaObed <91049664+BenayaObed@users.noreply.github.com>
2024-06-06 20:52:30 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -10,6 +10,7 @@ const createUnixSocketPool = async config => {
         password: process.env.DB_PASS, // e.g. 'my-db-password'
         database: process.env.DB_NAME, // e.g. 'my-database'
         socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
+        jwtsecret: process.env.JWT_SECRET
     });
 };
 
